877fa6d9c234df9045d2bbe90129bbe45c68e951
Will Brady <willgbrady@gmail.com>
2015-08-05 15:25:22 -0400

    Remove classify call

--- lib/activerecord/required_data.rb
+++ lib/activerecord/required_data.rb
@@ -2,7 +2,7 @@ module ActiveRecord
   module RequiredData
     def static_data(key, values)
       return if ENV['SKIP_REQUIRED_DATA'] == 'true'
-      actual_values = self.name.classify.constantize.all.map{|record| record.send(key)}
+      actual_values = self.name.constantize.all.map{|record| record.send(key)}
       missing_values = values - actual_values
       raise "Missing required #{key.to_s.pluralize} for #{self.name.classify}: #{missing_values}" if missing_values.any?
       extra_values = actual_values - values
